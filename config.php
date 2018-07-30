ff5f9360d06ea6181f483c69af91e6a9b188d443
minhphuongdk <32402333+minhphuongdk@users.noreply.github.com>
2018-07-30 16:45:18 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+return array(
+    /**
+     * Provide your MYSQL database credentials
+     */
+    'db_host' => 'localhost',
+    'db_username' => 'umenboo',
+    'db_name' => 'menboo',
+    'db_password' => 'Billgator13797',
+
+    /**
+     * Option to know if debug is enabled or not
+     */
+    'debug' => true,
+
+    /**
+     * Option to enable https
+     */
+    'https' => true,
+
+    /**
+     * cookie path
+     */
+    'cookie_path' => '/',
+
+    'api-key' => 'normalKey',
+
+    'rtl-langs' => array(
+        'ar',
+        'fa',
+    ),
+
+    'installed' => '1'
+
+);
